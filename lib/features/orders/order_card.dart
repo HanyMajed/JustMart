c2c25bdddf0eb55c4bd315561136f0d1ca14ae95
HanyMajed <hmsweidan@gmail.com>
2025-05-19 11:35:21 +0300

    complete the order card UI

--- lib/features/orders/order_card.dart
+++ lib/features/orders/order_card.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:just_mart/core/utils/app_colors.dart';
 import 'package:just_mart/core/utils/app_text_styles.dart';
 import 'package:just_mart/features/orders/order_model.dart';
+import 'package:just_mart/features/vendor_mode/widgets/appbar_for_vendor_views.dart';
+import 'package:just_mart/features/vendor_mode/widgets/product_item_model.dart';
 
 class OrderCard extends StatelessWidget {
   final OrderModel order;
@@ -13,92 +15,77 @@ class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: appbarForVendorViews(title: "الطلبات"),
       body: Padding(
-        padding: const EdgeInsets.only(top: 16),
-        child: SizedBox(
-          height: 300,
-          child: Column(
+        padding: const EdgeInsets.only(top: 8, right: 16, left: 16),
+        child: Container(
+          height: 175,
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 247, 247, 247),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
             children: [
               Container(
-                padding: const EdgeInsets.all(12),
+                width: 80,
+                height: 80,
                 decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  border: Border(
-                    bottom: BorderSide(color: Colors.grey.shade300),
-                  ),
+                  color: const Color.fromARGB(255, 204, 204, 204),
+                  borderRadius: BorderRadius.circular(50),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CircleAvatar(
-                        backgroundColor: Colors.grey.shade400,
-                        radius: 24,
-                        child: const Icon(
-                          Icons.shopping_bag,
-                          size: 28,
-                          color: AppColors.primaryColor,
-                        ),
-                      ),
-                      const SizedBox(width: 12),
-
-                      /// Main content
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'طلب رقم: #${order.orderId}',
-                              style: TextStyles.bold16.copyWith(color: Colors.grey.shade900),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-
-                            const SizedBox(height: 4),
-
-                            /// Order date
-                            Text(
-                              'تم الطلب: ${order.orderDate}',
-                              style: TextStyles.regular13.copyWith(color: const Color.fromARGB(255, 151, 151, 151)),
-                            ),
-
-                            const SizedBox(height: 8),
-
-                            /// Count and price
-                            Row(
-                              children: [
-                                Text(
-                                  'عدد المنتجات:',
-                                  style: TextStyles.bold16.copyWith(color: Colors.grey.shade900),
-                                ),
-                                const SizedBox(width: 4),
-                                Text(
-                                  '${order.orderItems.length}',
-                                  style: TextStyles.bold16.copyWith(color: Colors.grey.shade900),
-                                ),
-                                const Spacer(),
-                                Flexible(
-                                  child: Text(
-                                    '${order.totalPrice.toInt()} دينار',
-                                    style: TextStyles.bold16.copyWith(color: Colors.grey.shade900),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-
-                      const SizedBox(width: 8),
-                    ],
-                  ),
+                child: const Icon(
+                  Icons.shopping_bag_outlined,
+                  color: AppColors.primaryColor,
+                  size: 60,
                 ),
               ),
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'طلب رقم: #${order.orderId}',
+                    style: TextStyles.bold16.copyWith(color: Colors.grey.shade900),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'تم الطلب: ${order.orderDate.year}-${order.orderDate.month}-${order.orderDate.day} الساعة: ${order.orderDate.hour}:${order.orderDate.minute}',
+                    style: TextStyles.regular13.copyWith(color: const Color.fromARGB(255, 151, 151, 151)),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    'عدد المنتجات: #${order.orderItems.length}       السعر: ${order.totalPrice.toInt()} دينار',
+                    style: TextStyles.bold16.copyWith(color: Colors.grey.shade900),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    ' المنتجات: ${getItemsNames(order.orderItems)}',
+                    style: TextStyles.regular16.copyWith(color: Colors.grey.shade900),
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 2,
+                  ),
+                ],
+              ))
             ],
           ),
         ),
       ),
     );
+  }
+
+  String getItemsNames(List<ProductItemModel> p) {
+    List<String> names = [];
+    for (var element in p) {
+      names.add(element.productName);
+    }
+    return names.join(', ');
   }
 }

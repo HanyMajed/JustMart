eed073977c82cba837dea643202524c913501e56
HanyMajed <hmsweidan@gmail.com>
2025-05-19 23:40:10 +0300

    title  edit on home page

--- lib/features/home/presentation/views/widgets/best_selling_header.dart
+++ lib/features/home/presentation/views/widgets/best_selling_header.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:just_mart/core/utils/app_text_styles.dart';
 import 'package:just_mart/features/best_selling_product/presentation/views/best_selling_view.dart';
 
-class BestSellingHeader extends StatelessWidget {
-  BestSellingHeader({super.key, required this.text});
+class SellingHeader extends StatelessWidget {
+  SellingHeader({super.key, required this.text});
   String text;
   @override
   Widget build(BuildContext context) {
